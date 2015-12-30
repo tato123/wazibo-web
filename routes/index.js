40048d587b42245aa6bfc3eaeeff42e5f4e176c2
Jonathan <fontajo@yahoo.com>
2015-12-30 16:09:25 -0500

    updates to web design for better buyer experience

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,39 @@
 'use strict';
 
-var express = require('express'), 
-	router = express.Router(),
-	wzapi = require('../client/wzapi'),
-	isAuthenticated = require('../middleware/isAuthenticated'),
-	logger = require('../logger');
+var express = require('express'),
+  router = express.Router(),
+  wzapi = require('../client/wzapi'),
+  isAuthenticated = require('../middleware/isAuthenticated'),
+  logger = require('../logger');
 
 router.get('/', function(req, res) {
-	wzapi
-		.user(req.user)
-		.getEvent(function(events) {
-			logger.info('the user', req.user);
-			logger.info('events', events);
-			res.render('home', {items: events, user:req.user});	
-		});		
-}); 
+  wzapi
+    .user(req.user)
+    .getEvent(function(events) {
+      logger.info('the user', req.user);
+      logger.info('events', events);
+
+      var items = [];
+      for (var x = 0; x < 6; x++) {
+        items.push(events[0]);
+      }
+      res.render('pages/home', {
+        items: items,
+        user: req.user
+      });
+    });
+});
 
 router.get('/login', function(req, res) {
-	res.render('login');
+  res.render('pages/login');
+});
+
+router.get('/about', function(req, res) {
+   res.render('pages/about'); 
+});
+
+router.get('/terms', function(req, res) {
+    res.render('pages/terms');
 });
 
 module.exports = router;
--- routes/sell.js
+++ routes/sell.js
@@ -1,73 +1,70 @@
 'use strict';
 
 var express = require('express'),
-	router = express.Router(),
-	wzapi = require('../client/wzapi'),
-	isAuthenticated = require('../middleware/isAuthenticated'),
-	logger = require('../logger');
+    router = express.Router(),
+    wzapi = require('../client/wzapi'),
+    isAuthenticated = require('../middleware/isAuthenticated'),
+    logger = require('../logger');
 
 /**
  * @description
  * Manage all the things that you are selling
  */
 router.get('/', isAuthenticated, function (req, res) {
-	res.render('sell-manage', { user: req.user, message:req.flash('error') });
+    res.render('sell-manage', { user: req.user, message: req.flash('error') });
 });
 
-
-router.route('/event')
 /**
  * @description 
  * Get the page for setting up a new event
  */
-	.get(isAuthenticated, function (req, res) {
-		res.render('sell-event', { user: req.user });
-	})
+router.get('/event', isAuthenticated, function (req, res) {
+    res.render('sell-event', { user: req.user });
+})
 	
 /**
 * @description
 * Handles parsing and sending an event to the api server to be
 * saved as a new event by our user
 * */
-	.post(isAuthenticated, function (req, res) {
-		wzapi
-			.user(req.user)
-			.saveEvent(req.body, function (event) {
-				res.redirect('/sell/item?eventId=' + event._id);
-				/*
-				.catch(function(error) {
-					logger.error('Error occured saving event',error);
-					req.flash('error', 'Oops looks like we couldn\'t save your event');
-					res.render('sell-event', {message:req.flash('error')});
-				});*/
-			});
+router.post('/event', isAuthenticated, function (req, res) {
+    wzapi
+        .user(req.user)
+        .saveEvent(req.body, function (event) {
+            res.redirect('/sell/item?eventId=' + event._id);
+            /*
+            .catch(function(error) {
+                logger.error('Error occured saving event',error);
+                req.flash('error', 'Oops looks like we couldn\'t save your event');
+                res.render('sell-event', {message:req.flash('error')});
+            });*/
+        });
 
-	});
+});
 
-router.route('/item')
 /** 
  * @description 
  * Get the page for setting up a new event
  */
-	.get(isAuthenticated, function (req, res) {		
-		wzapi
-			.user(req.user)
-			.getMediaBucket(function(bucket) {
-				res.render('sell-item', { user: req.user, eventId: req.query.eventId, bucket: bucket });		
-			});				
-	})
+router.get('/item', isAuthenticated, function (req, res) {
+    wzapi
+        .user(req.user)
+        .getMediaBucket(function (bucket) {
+            res.render('sell-item', { user: req.user, eventId: req.query.eventId, bucket: bucket });
+        });
+})
 /**
 * @description
 * Handles parsing and sending an event to the api server to be
 * saved as a new event by our user
 * */
-	.post(isAuthenticated, function (req, res) {
-		
-		wzapi
-			.user(req.user)
-			.saveItem(req.query.eventId, req.body, function(item) {
-				res.redirect('/sell');		
-			});		
-	});
+router.post('/item', isAuthenticated, function (req, res) {
+
+    wzapi
+        .user(req.user)
+        .saveItem(req.query.eventId, req.body, function (item) {
+            res.redirect('/sell');
+        });
+});
 
 module.exports = router;
